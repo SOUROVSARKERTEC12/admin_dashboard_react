472dce449f947379335f6ed1e22337e80abd665c
SOUROV <sourovsarker005@gmail.com>
2023-03-05 16:12:25 +0600

    Sidebar and FeaturedInfo Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
+    <div>
+        <Topbar/>
+        <div className="container">
+            <Sidebar/>
+            <Home/>
+        </div>
     </div>
   );
 }
