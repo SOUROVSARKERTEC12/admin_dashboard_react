3418cc9531f730d57749eb77e7a2fde2ac522099
SOUROV <sourovsarker005@gmail.com>
2023-03-07 14:32:04 +0600

    UserList Table Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Sidebar from "./components/sidebar/Sidebar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 import "./App.css"
 
 function App() {
-  return (
-    <div>
-        <Topbar/>
-        <div className="container">
-            <Sidebar/>
-            <Home/>
-        </div>
-    </div>
-  );
+    return (
+        <Router>
+            <Topbar/>
+            <div className="container">
+                <Sidebar/>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route path="/users">
+                        <UserList/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
