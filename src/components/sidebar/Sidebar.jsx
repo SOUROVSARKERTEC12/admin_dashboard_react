53e33a9d6ee9d504cb62e089200211fb6711cd5e
SOUROV <sourovsarker005@gmail.com>
2023-03-10 00:03:36 +0600

    Admin Panel Complete

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -14,6 +14,7 @@ import {
     WorkOutline,
     Report,
 } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
     return (
@@ -22,17 +23,18 @@ function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <LineStyle className="sidebarIcon"/>
-                            Home
-                        </li>
-
+                        <Link to="/" className="link">
+                            <li className="sidebarListItem active">
+                                <LineStyle className="sidebarIcon" />
+                                Home
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
-                            <Timeline className="sidebarIcon"/>
+                            <Timeline className="sidebarIcon" />
                             Analytics
                         </li>
                         <li className="sidebarListItem">
-                            <TrendingUp className="sidebarIcon"/>
+                            <TrendingUp className="sidebarIcon" />
                             Sales
                         </li>
                     </ul>
@@ -40,22 +42,24 @@ function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-
+                        <Link to="/users" className="link">
+                            <li className="sidebarListItem">
+                                <PermIdentity className="sidebarIcon" />
+                                Users
+                            </li>
+                        </Link>
+                        <Link to="/products" className="link">
+                            <li className="sidebarListItem">
+                                <Storefront className="sidebarIcon" />
+                                Products
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
-                            <PermIdentity className="sidebarIcon"/>
-                            Users
-                        </li>
-
-                        <li className="sidebarListItem">
-                            <Storefront className="sidebarIcon"/>
-                            Products
-                        </li>
-                        <li className="sidebarListItem">
-                            <AttachMoney className="sidebarIcon"/>
+                            <AttachMoney className="sidebarIcon" />
                             Transactions
                         </li>
                         <li className="sidebarListItem">
-                            <BarChart className="sidebarIcon"/>
+                            <BarChart className="sidebarIcon" />
                             Reports
                         </li>
                     </ul>
@@ -64,15 +68,15 @@ function Sidebar() {
                     <h3 className="sidebarTitle">Notifications</h3>
                     <ul className="sidebarList">
                         <li className="sidebarListItem">
-                            <MailOutline className="sidebarIcon"/>
+                            <MailOutline className="sidebarIcon" />
                             Mail
                         </li>
                         <li className="sidebarListItem">
-                            <DynamicFeed className="sidebarIcon"/>
+                            <DynamicFeed className="sidebarIcon" />
                             Feedback
                         </li>
                         <li className="sidebarListItem">
-                            <ChatBubbleOutline className="sidebarIcon"/>
+                            <ChatBubbleOutline className="sidebarIcon" />
                             Messages
                         </li>
                     </ul>
@@ -81,15 +85,15 @@ function Sidebar() {
                     <h3 className="sidebarTitle">Staff</h3>
                     <ul className="sidebarList">
                         <li className="sidebarListItem">
-                            <WorkOutline className="sidebarIcon"/>
+                            <WorkOutline className="sidebarIcon" />
                             Manage
                         </li>
                         <li className="sidebarListItem">
-                            <Timeline className="sidebarIcon"/>
+                            <Timeline className="sidebarIcon" />
                             Analytics
                         </li>
                         <li className="sidebarListItem">
-                            <Report className="sidebarIcon"/>
+                            <Report className="sidebarIcon" />
                             Reports
                         </li>
                     </ul>
